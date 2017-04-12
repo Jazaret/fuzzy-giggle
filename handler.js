7b6843c9aedceec5c4a48e5facbd701246553b91
Javier Azaret <jazaret@qqsolutions.com>
2017-04-12 10:46:24 -0400

    addTask

--- handler.js
+++ handler.js
@@ -1,7 +1,7 @@
 'use strict';
 
 console.log('Loading function');
-//const uuid = require('node-uuid');
+const uuid = require('node-uuid');
 const doc = require('dynamodb-doc');
 const dynamo = new doc.DynamoDB();
 
@@ -10,7 +10,7 @@ module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
+      message: 'Go Serverless v1.0! Your function executed successfully! ' + uuid.v1(),
       input: event,
     }),
   };
@@ -37,3 +37,40 @@ module.exports.getTasks = (event, context, callback) => {
         callback(null, response);
     });
 };
+
+module.exports.putTasks = (event, context, callback) => {
+
+    var newId = uuid.v1();
+    console.log('id = ' + newId);
+
+    if (!event.name || !event.description) {
+        response = {
+          statusCode: 200,
+          body: (!event.name) ? "Name is required" : "Description is required"          
+        };        
+        callback(null, response);
+        return;
+    }
+
+    var newTask = {
+      newId: newid,
+      name: name,
+      description: description,
+      priority: priority,
+      completed: completed
+    }
+    
+    var params = {
+        Item: event,
+        TableName: process.env.TABLE_NAME
+    };
+    
+    dynamo.putItem(params, function(err, data) {
+        if (err) {
+            callback(err, null);
+        } else {
+            callback(null, data);
+        }
+    });
+};
+
