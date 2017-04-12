937bc0717bcf874fb05097394c7ab7aa3efa3c63
Javier Azaret <jazaret@qqsolutions.com>
2017-04-12 12:30:10 -0400

    UpdateTask method changes

--- handler.js
+++ handler.js
@@ -24,91 +24,121 @@ module.exports.getTasks = (event, context, callback) => {
 
   var result, response;
 
-  dynamo.scan({ TableName: tableName }, function(err, data) {
-        if (err) {
-            callback(err, null);
-        } else {
-            response = {
-                statusCode: 200,
-                body: JSON.stringify({
-                  message: data
-                }),
-              };              
-            callback(null, response);  
-        }
-    });
+  dynamo.scan({ TableName: tableName }, function (err, data) {
+    if (err) {
+      callback(err, null);
+    } else {
+      response = {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: data
+        }),
+      };
+      callback(null, response);
+    }
+  });
 };
 
 module.exports.addTask = (event, context, callback) => {
-    var response;
-    var newId = uuid.v1();
-    console.log('id = ' + newId);
+  var response;
+  var newId = uuid.v1();
+  console.log('id = ' + newId);
 
-    if (!event.name || !event.description) {
-        response = {
-          statusCode: 400,
-          body: (!event.name) ? "Name is required" : "Description is required"          
-        };        
-        callback(null, response);
-        return;
-    }
-    
-    var params = {
-      Item: {
-        taskId: newId,
-        name: event.name,
-        description: event.description
-      },
-      TableName: tableName
+  if (!event.name || !event.description) {
+    response = {
+      statusCode: 400,
+      body: (!event.name) ? 'Name is required' : 'Description is required'
     };
-    
-    dynamo.putItem(params, function(err, data) {
-        if (err) {
-            callback(err, null);
-        } else {
-            response = {
-                statusCode: 200,
-                body: JSON.stringify({
-                  message: data
-                }),
-              };              
-            callback(null, response);
-        }
-    });
+    callback(null, response);
+    return;
+  }
+
+  var params = {
+    Item: {
+      taskId: newId,
+      name: event.name,
+      description: event.description
+    },
+    TableName: tableName
+  };
+
+  dynamo.putItem(params, function (err, data) {
+    if (err) {
+      callback(err, null);
+    } else {
+      response = {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: data
+        }),
+      };
+      callback(null, response);
+    }
+  });
 };
 
 module.exports.updateTask = (event, context, callback) => {
-    var response;
-    
-    if (!event.name || !event.description) {
-        response = {
-          statusCode: 400,
-          body: (!event.name) ? "Name is required" : "Description is required"          
-        };        
-        callback(null, response);
-        return;
-    }
-    
-    var params = {
-      Item: {
-        taskId: event.taskId,
-        name: event.name,
-        description: event.description
-      },
-      TableName: tableName
+  var response, params;
+  if (!event.name || !event.description) {
+    response = {
+      statusCode: 400,
+      body: (!event.name) ? 'name is required' : 'description is required'
     };
-    
-    dynamo.updateItem(params, function(err, data) {
-        if (err) {
+    callback(null, response);
+    return;
+  }
+
+  //GetItem
+  params = {
+    TableName: tableName,
+    Key: {
+      taskId: event.taskId
+    },
+    AttributesToGet: ['taskId']
+  };
+
+  dynamo.getItem(params, function (err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data);
+      if (!data.Item) {
+        response = {
+          statusCode: 404,
+          body: JSON.stringify({
+            message: 'Task not found'
+          }),
+        };
+        callback(null, response);
+      } else {
+        //Item found, now update
+        var params = {
+          Item: {
+            taskId: data.Item.taskId,
+            name: event.name,
+            description: event.description
+          },
+          TableName: tableName
+        };
+
+        dynamo.putItem(params, function (err, data) {
+          if (err) {
             callback(err, null);
-        } else {
+          } else {
             response = {
-                statusCode: 200,
-                body: JSON.stringify({
-                  message: data
-                }),
-              };              
+              statusCode: 200,
+              body: JSON.stringify({
+                message: data
+              }),
+            };
             callback(null, response);
-        }
-    });
+          }
+        });
+      }
+    }
+  });
+  //End GetItem
+  return;
+
+
 };
