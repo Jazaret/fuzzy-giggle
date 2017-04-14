b1067109e6edd488e01a13141134d49a4408fa69
Javier Azaret <jazaret@gmail.com>
2017-04-14 15:24:32 -0400

    add methods for upsert messages feature

--- handler.js
+++ handler.js
@@ -203,17 +203,17 @@ module.exports.emailTasks = (event, context, callback) => {
     } else {
       console.log(data);
       data.Items.forEach(function (value) {
-          //check if recipient already exists
-          if (!emailsToSend[value.user]) {
-            //if recipient doesn't exist then add to master list
-            uniqueNames.push(value.user);
-            //initialize dictionary entry
-            emailsToSend[value.user] = [];
-          }
-          //add task to email
-          emailsToSend[value.user].push({
-            description: value.description
-          });
+        //check if recipient already exists
+        if (!emailsToSend[value.user]) {
+          //if recipient doesn't exist then add to master list
+          uniqueNames.push(value.user);
+          //initialize dictionary entry
+          emailsToSend[value.user] = [];
+        }
+        //add task to email
+        emailsToSend[value.user].push({
+          description: value.description
+        });
       });
 
       console.log(uniqueNames);
@@ -248,6 +248,122 @@ module.exports.emailTasks = (event, context, callback) => {
 
 
 };
+
+//Method to handle messages to and from users.
+//Creator of message can specify whether recipient has access to modify message
+module.exports.upsertMessage = (event, context, callback) => {
+  console.log(context);
+  var response, message;
+  var contextUserId = (context.identity) ? context.identity.cognitoIdentityId : context.awsRequestId;
+  var messageTable = 'message-table';
+  var isNewMessage = !event.messageId;
+
+  if (isNewMessage) {
+    message = formatMessageToAdd(event);
+  }
+
+  var upsertParams = {
+    tableName: messageTable,
+    Item: {}
+  };
+
+  var getParams = {
+    TableName: tableName,
+    Key: {
+      messageId: event.messageId
+    }
+  }
+
+  return; //NOT DONE YET
+
+  if (isNewMessage) {
+    // add message to db
+    dynamo.putItem(params, function (err, data) {
+      if (err) {
+        callback(err, null);
+      } else {
+        response = {
+          statusCode: 200,
+          body: JSON.stringify({
+            message: data
+          }),
+        };
+        callback(null, response);
+      }
+    });
+  } else {
+    //get original message
+    dynamo.getItem(getParams, function (err, data) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(data);
+        if (!data.Item) {
+          response = {
+            statusCode: 404,
+            body: JSON.stringify({
+              message: 'Message not found'
+            }),
+          };
+          callback(null, response);
+        } else {
+          var messageItem = formatMessageToUpdate(event.message, data)
+          response = updateMessage(messageItem, contextUserId, messageTable);
+          callback(null, response);
+        }
+      }
+    });
+  }
+};
+
+//updates the message can be from originator or recipient
+function updateMessage(messageItem, contextUserId, messageTable) {
+
+  //proceed only if editor is originator or recipient has rights to edit
+  var canProceed = (contextUserId === messageItem.fromUserId || (messageItem.allowEdit && contextUserId === messageItem.toUserId));
+
+  if (!canProceed) {
+    response = {
+      statusCode: 401,
+      body: "user not authorized to edit message"
+    };
+    return response;
+  }
+
+  var params = {
+    TableName: messageTable,
+    Key: {
+      "messageId": messageItem.messageId
+    },
+    UpdateExpression: "set message = :m, updateDate=:d",
+    ExpressionAttributeValues: {
+      ":m": messageItem.message,
+      ":d": new Date().toISOString
+    },
+    ReturnValues: "UPDATED_NEW"
+  };
+
+  //update message
+  dynamo.updateItem(params, function (err, data) {
+    if (err) {
+      callback(err, null);
+    } else {
+      response = {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: data
+        }),
+      };
+    }
+  });
+
+  //recpient edited message, notify source.
+  if (contextUserId === messageItem.fromUserId) {
+    //get email from ID & send email
+  }
+
+  return response;
+}
 
 //validates the properties of a task
 function validateTask(task) {
@@ -293,7 +409,7 @@ function sortTasks(tasks) {
   });
 
   return tasks;
-}
+};
 
 //prepare task to be safe for add/update
 function formatTaskToUpsert(task) {
@@ -308,4 +424,29 @@ function formatTaskToUpsert(task) {
   }
 
   return result;
-}
+};
+
+//input model = messageId (optional), message, toUserId, AllowEdit
+function formatMessageToAdd(messageItem, contextUserId) {
+  var utcNow = new Date().toISOString();
+  var result = {
+    messageId: messageItem.messageId,
+    message: messageItem.message,
+    toUserId: messageItem.toUserId,
+    fromUserId: contextUserId,
+    allowEdit: !!messageItem.allowEdit,
+    dateCreated: utcNow,
+    dateUpdated: utcNow
+  }
+};
+
+function formatMessageToUpdate(newMessageText, oldData) {
+  return {
+    messageId: oldData.messageId,
+    allowEdit: oldData.allowEdit,
+    toUserId: oldData.toUserId,
+    fromUserId: oldData.fromUserId,
+    message: newMessageText,
+    dateUpdated: new Date().toISOString()
+  };
+};
