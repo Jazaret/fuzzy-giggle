56aebedbee3238b3790ac5a49b0b30dd36924b47
Javier Azaret <jazaret@gmail.com>
2017-04-19 14:16:40 -0400

    update addTask tests to validate completed exists/not exists

--- tests/TaskAdd/AddTask_Success.js
+++ tests/TaskAdd/AddTask_Success.js
@@ -10,26 +10,50 @@ const db = {
 
 let tasks = new Tasks(db);
 
-const taskToAdd = {
+const taskToAddNoCompleted = {
             "user": "jazaret@gmail.com",
             "description": "hey there",
             "priority": 0
         };   
 
+const taskToAddWithCompleted = {
+            "user": "jazaret@gmail.com",
+            "description": "hey there",
+            "priority": 0,
+            "completed": "2016-07-06T12:22:46-04:00"
+        }; 
 
 
-describe('AddTask_Successful_Add', function () {
-    it('Task is added', function (done) {
+describe('AddTask_Successful_Add_CompletedValue_NotExist', function () {
+    it('Task is added without completed', function (done) {
         
         var callback = function (err, response) {
             expect(response.statusCode).to.equal(200);
             var taskAdded = JSON.parse(response.body).message;                        
-            expect(taskAdded.user).to.equal(taskToAdd.user);
-            expect(taskAdded.description).to.equal(taskToAdd.description);
-            expect(taskAdded.priority).to.equal(taskToAdd.priority);
+            expect(taskAdded.user).to.equal(taskToAddNoCompleted.user);
+            expect(taskAdded.description).to.equal(taskToAddNoCompleted.description);
+            expect(taskAdded.priority).to.equal(taskToAddNoCompleted.priority);
+            expect(taskAdded.completed).to.be.undefined;
             expect(taskAdded.taskId).to.be.ok;
             done();
         };
-        tasks.addTask(taskToAdd, callback);
+        tasks.addTask(taskToAddNoCompleted, callback);
+    });
+});
+
+describe('AddTask_Successful_Add_CompletedValue_Exist', function () {
+    it('Task is added with completed', function (done) {
+        
+        var callback = function (err, response) {
+            expect(response.statusCode).to.equal(200);
+            var taskAdded = JSON.parse(response.body).message;                        
+            expect(taskAdded.user).to.equal(taskToAddWithCompleted.user);
+            expect(taskAdded.description).to.equal(taskToAddWithCompleted.description);
+            expect(taskAdded.priority).to.equal(taskToAddWithCompleted.priority);
+            expect(taskAdded.completed).to.be.equal(taskToAddWithCompleted.completed);
+            expect(taskAdded.taskId).to.be.ok;
+            done();
+        };
+        tasks.addTask(taskToAddWithCompleted, callback);
     });
 });
